47071cdd66af857a7763508d9e1df304012b9eb8
coltenappleby <colten.appleby@gmail.com>
2021-04-13 17:41:50 -0400

    done but a struggle

--- index.js
+++ index.js
@@ -3,4 +3,42 @@ document.addEventListener("DOMContentLoaded", function() {
   init();
 
   // ADD CODE HERE!
+
+  // Effectively Global Variables
+  const movesContainer = document.querySelector("ul#moves-container")
+  const moveButton = document.querySelector('button#move-button')
+
+  document.addEventListener("keydown", event => {
+    if (event.key.startsWith("Arrow")) {
+      dir = event.key.slice(5).toLocaleLowerCase()
+      li = document.createElement("li")
+      li.textContent = dir
+      movesContainer.append(li)
+    }
+  })
+
+  //Move Button
+ 
+  moveButton.addEventListener('click', event => {
+    const nextMoveLi = movesContainer.querySelector('li')
+
+    move(nextMoveLi.textContent)
+
+    nextMoveLi.remove()
+
+    console.log(nextMoveLi)
+  })
+
+  // Delete a Move
+
+  movesContainer.addEventListener("click", event => {
+
+      if (event.target.tagName === "LI" ){
+        event.target.remove()
+      }
+
+
+  })
+
+  
 });
